dfbf538829629b092b88b2a0ae2e530b678eeabd
Madhurrai <madhursrai45@gmail.com>
2023-10-09 12:04:00 +0530

    Python Commit

---
+++ programs/Assignment_12_2.py
@@ -0,0 +1,21 @@
+def supress_exception(exception = KeyError, result = False):
+    def dummy_return(func):
+        def inner(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except exception as e:
+                if result is not None:
+                    return result
+                else:
+                    raise e
+        return inner
+    return dummy_return
+
+users = {'Madhur': 'Madhur@123', 'ABC': 'xyz' }
+@supress_exception(KeyError, result=False)
+def authenticate(user, password):
+    print(f'Authenticating {user}')
+    return users.get(user) == password
+
+result = authenticate('Madhur', 'Madhur@123')
+print("Authentication result: ", result)
